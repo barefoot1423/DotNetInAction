e266195a94fc6fdfaf658daa632d9efd70b58fea
Hyunwoo Cho <barefoot1423@gmail.com>
2025-04-24 22:35:19 -0400

    Listing 2.4

---
+++ DotNetInAction/Chapter2/Program.cs
@@ -0,0 +1,9 @@
+﻿using Figgle;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: HelloDotnet <text>");
+    Environment.Exit(1);
+}
+
+Console.WriteLine(FiggleFonts.Standard.Render(args[0]));
